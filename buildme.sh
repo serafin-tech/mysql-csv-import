b3631f005421230c8f69a56739e780e13cd1490f
Jaroslaw Wencel <jaroslaw.wencel@gmail.com>
2024-05-31 23:27:19 +0200

    script for building process

---
+++ buildme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ ! -d venv ]]
+then
+  python3.11 -m venv venv
+fi
+
+source venv/bin/activate
+
+python3.11 -m pip install -U pip
+python3.11 -m pip install -r requirements.txt
+
+python3 -m pip install -U build setuptools setuptools-scm
+
+rm -rf dist/ *.egg-info/
+
+python3 -m setuptools_scm
+
+python3 -m build .
